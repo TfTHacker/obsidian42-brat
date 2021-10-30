86473529bbb8f8e18782b6f9899846789f864fd1
CK <69121180+TfTHacker@users.noreply.github.com>
2021-10-30 16:33:34 +0200

    improved esbuild process to include copying manifest.json and doing eslint

---
+++ esbuild.config.mjs
@@ -0,0 +1,34 @@
+import esbuild from "esbuild";
+import process from "process";
+import fs from "fs";
+import { ESLint } from "eslint";
+
+const prod = (process.argv[2] === "production");
+
+esbuild.build({
+    entryPoints: ["src/main.ts"],
+    bundle: true,
+    external: ["obsidian"],
+    format: "cjs",
+    watch: !prod,
+    target: "es2016",
+    logLevel: "info",
+    sourcemap: prod ? false : "inline",
+    treeShaking: true,
+    outfile: "main.js",
+}).catch(() => process.exit(1));
+
+fs.copyFile("manifest.json", "build/manifest.json", (err) => {if(err) console.log(err )} );
+
+// eslint won't slow down the build process, just runs after the build finishes
+(async function eslintTest() {
+  const eslint = new ESLint();
+  const results = await eslint.lintFiles(["src/**/*.ts"]);
+  const formatter = await eslint.loadFormatter("stylish");
+  const resultText = formatter.format(results);
+  console.log(resultText);
+})().catch((error) => {
+  process.exitCode = 1;
+  console.error(error);
+});
+
