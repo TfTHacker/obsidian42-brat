7c1cc3a52e3844760a080e546d121053b0aa9793
TfTHacker <69121180+TfTHacker@users.noreply.github.com>
2022-02-13 11:48:51 +0100

    added minify to build option

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -15,6 +15,7 @@ esbuild.build({
     logLevel: "info",
     sourcemap: prod ? false : "inline",
     treeShaking: true,
+    minify: true,
     outfile: "build/main.js",
 }).catch(() => process.exit(1));
 
