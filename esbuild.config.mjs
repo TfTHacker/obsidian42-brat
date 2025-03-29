3a34b36f7989b7a64d53088e5eb25f3713de1128
Johann Richard <189003+johannrichard@users.noreply.github.com>
2025-03-29 20:37:48 +0100

    build: 🐛 copy files after `build/` directory is created on build
    when `build/` does not exist, `copyFile()` calls fail. This change fixes that.

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,49 +1,53 @@
-import console from "node:console";
-import fs from "node:fs";
 import process from "node:process";
 import builtins from "builtin-modules";
 import esbuild from "esbuild";
-
-fs.copyFile("manifest.json", "build/manifest.json", (err) => {
-	if (err) console.log(err);
-});
-fs.copyFile("styles.css", "build/styles.css", (err) => {
-	if (err) console.log(err);
-});
+import copy from "esbuild-copy-static-files";
 
 const prod = process.argv[2] === "production";
 
 const context = await esbuild.context({
-	entryPoints: ["src/main.ts"],
-	bundle: true,
-	minify: prod,
-	external: [
-		"obsidian",
-		"electron",
-		"@codemirror/autocomplete",
-		"@codemirror/collab",
-		"@codemirror/commands",
-		"@codemirror/language",
-		"@codemirror/lint",
-		"@codemirror/search",
-		"@codemirror/state",
-		"@codemirror/view",
-		"@lezer/common",
-		"@lezer/highlight",
-		"@lezer/lr",
-		...builtins,
-	],
-	format: "cjs",
-	target: "es2018",
-	logLevel: "info",
-	sourcemap: prod ? false : "inline",
-	treeShaking: true,
-	outfile: "build/main.js",
+  entryPoints: ["src/main.ts"],
+  bundle: true,
+  minify: prod,
+  external: [
+    "obsidian",
+    "electron",
+    "@codemirror/autocomplete",
+    "@codemirror/collab",
+    "@codemirror/commands",
+    "@codemirror/language",
+    "@codemirror/lint",
+    "@codemirror/search",
+    "@codemirror/state",
+    "@codemirror/view",
+    "@lezer/common",
+    "@lezer/highlight",
+    "@lezer/lr",
+    ...builtins,
+  ],
+  format: "cjs",
+  target: "es2018",
+  logLevel: "info",
+  sourcemap: prod ? false : "inline",
+  treeShaking: true,
+  outfile: "build/main.js",
+  plugins: [
+    copy({
+      src: "manifest.json",
+      dest: "build/manifest.json",
+      watch: !prod,
+    }),
+    copy({
+      src: "styles.css",
+      dest: "build/styles.css",
+      watch: !prod,
+    }),
+  ],
 });
 
 if (prod) {
-	await context.rebuild();
-	process.exit(0);
+  await context.rebuild();
+  process.exit(0);
 } else {
-	await context.watch();
+  await context.watch();
 }
