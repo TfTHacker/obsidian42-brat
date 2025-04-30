710779b66a177fce50c7984519b3a305a919bef3
Johann Richard <189003+johannrichard@users.noreply.github.com>
2025-04-30 07:33:05 +0200

    build: :sparkles: only write new minAppVersion requirements to versions.json
    We only need to update versions.json if the minAppVersion for the plugin changes.

--- version-bump.mjs
+++ version-bump.mjs
@@ -9,6 +9,9 @@ manifest.version = targetVersion;
 writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'));
 
 // update versions.json with target version and minAppVersion from manifest.json
+// but only if the target version is not already in versions.json
 const versions = JSON.parse(readFileSync('versions.json', 'utf8'));
+if (!Object.values(versions).includes(minAppVersion)) {
 versions[targetVersion] = minAppVersion;
 writeFileSync('versions.json', JSON.stringify(versions, null, '\t'));
+}
