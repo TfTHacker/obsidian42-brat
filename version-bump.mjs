a4673d23b3c2d3e27e0feee2728c5a51d05c31bd
Johann Richard <189003+johannrichard@users.noreply.github.com>
2025-05-02 07:55:42 +0200

    build: :construction_worker: beef-up `version-bump.mjs`
    avoid bumping `manifest.json` for pre-releases

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,17 +1,24 @@
-import { readFileSync, writeFileSync } from 'node:fs';
+import { readFileSync, writeFileSync } from "node:fs";
+const semver = await import("semver");
 
 const targetVersion = process.env.npm_package_version;
+const targetSemver = semver.parse(targetVersion);
 
 // read minAppVersion from manifest.json and bump version to target version
-const manifest = JSON.parse(readFileSync('manifest.json', 'utf8'));
+const manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 const { minAppVersion } = manifest;
+// Write manifest.json with target version only if the target version is not a pre-release version
+if (targetSemver.prerelease.length === 0) {
 manifest.version = targetVersion;
-writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'));
+  writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+} else {
+    console.warning(`Skipping version bump in manifest.json for pre-release version: ${targetVersion}`);
+}
 
 // update versions.json with target version and minAppVersion from manifest.json
 // but only if the target version is not already in versions.json
-const versions = JSON.parse(readFileSync('versions.json', 'utf8'));
+const versions = JSON.parse(readFileSync("versions.json", "utf8"));
 if (!Object.values(versions).includes(minAppVersion)) {
 versions[targetVersion] = minAppVersion;
-writeFileSync('versions.json', JSON.stringify(versions, null, '\t'));
+  writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
 }
