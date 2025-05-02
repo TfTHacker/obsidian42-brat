7d5e2558dd6e3d0fc654b8476d07a75311c2e8c6
Johann Richard <189003+johannrichard@users.noreply.github.com>
2025-05-02 17:36:10 +0200

    build: :construction_worker: don't bump `manifest.json` and `versions.json` pre-releases
    this avoid cluttering `versions.json` with pre-release versions.

--- version-bump.mjs
+++ version-bump.mjs
@@ -9,16 +9,18 @@ const manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 const { minAppVersion } = manifest;
 // Write manifest.json with target version only if the target version is not a pre-release version
 if (targetSemver.prerelease.length === 0) {
-manifest.version = targetVersion;
+  manifest.version = targetVersion;
   writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
-} else {
-    console.warning(`Skipping version bump in manifest.json for pre-release version: ${targetVersion}`);
-}
 
-// update versions.json with target version and minAppVersion from manifest.json
-// but only if the target version is not already in versions.json
-const versions = JSON.parse(readFileSync("versions.json", "utf8"));
-if (!Object.values(versions).includes(minAppVersion)) {
-versions[targetVersion] = minAppVersion;
-  writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+  // update versions.json with target version and minAppVersion from manifest.json
+  // but only if the target version is not already in versions.json
+  const versions = JSON.parse(readFileSync("versions.json", "utf8"));
+  if (!Object.values(versions).includes(minAppVersion)) {
+    versions[targetVersion] = minAppVersion;
+    writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+  }
+} else {
+  console.warning(
+    `Skipping version bump in manifest.json for pre-release version: ${targetVersion}`
+  );
 }
