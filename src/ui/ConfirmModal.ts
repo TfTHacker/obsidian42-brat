7ac6d845416e1652c0c3b368d72fdb7ac456f9da
Johann Richard <189003+johannrichard@users.noreply.github.com>
2025-09-07 17:05:48 +0200

    feat: ✨ let user override install of incompatible plugins
    user can select to continue installing plugins whose minAppVersion is too high for the current Obsidian
    
    CLOSES #124

--- src/ui/ConfirmModal.ts
+++ src/ui/ConfirmModal.ts
@@ -1,63 +1,67 @@
-import type { App } from 'obsidian';
-import { ButtonComponent, Modal } from 'obsidian';
+import type { App } from "obsidian";
+import { ButtonComponent, Modal } from "obsidian";
 
 interface ConfirmOptions {
-    app: App;
-    cancelButtonText?: string;
-    cssClass?: string;
-    message: DocumentFragment | string;
-    okButtonText?: string;
-    title?: DocumentFragment | string;
+	app: App;
+	cancelButtonText?: string;
+	cssClass?: string;
+	message: DocumentFragment | string;
+	okButtonText?: string;
+	title?: DocumentFragment | string;
 }
 
-type PromiseResolve<T> = undefined extends T ? (value?: PromiseLike<T> | T) => void
-    : (value: PromiseLike<T> | T) => void;
+type PromiseResolve<T> = undefined extends T
+	? (value?: PromiseLike<T> | T) => void
+	: (value: PromiseLike<T> | T) => void;
 
 class ConfirmModal extends Modal {
-    private isConfirmed = false;
-    private options: Required<ConfirmOptions>;
+	private isConfirmed = false;
+	private options: Required<ConfirmOptions>;
 
-    public constructor(options: ConfirmOptions, private readonly resolve: PromiseResolve<boolean>) {
-        super(options.app);
-        const DEFAULT_OPTIONS: Required<ConfirmOptions> = {
-            app: options.app,
-            cancelButtonText: 'Cancel',
-            cssClass: '',
-            message: options.message,
-            okButtonText: 'OK',
-            title: ''
-        };
-        this.options = { ...DEFAULT_OPTIONS, ...options };
-        this.containerEl.addClass('confirm-modal');
-    }
+	public constructor(
+		options: ConfirmOptions,
+		private readonly resolve: PromiseResolve<boolean>,
+	) {
+		super(options.app);
+		const DEFAULT_OPTIONS: Required<ConfirmOptions> = {
+			app: options.app,
+			cancelButtonText: "Cancel",
+			cssClass: "",
+			message: options.message,
+			okButtonText: "OK",
+			title: "",
+		};
+		this.options = { ...DEFAULT_OPTIONS, ...options };
+		this.containerEl.addClass("confirm-modal");
+	}
 
-    public override onClose(): void {
-        super.onClose();
-        this.resolve(this.isConfirmed);
-    }
+	public override onClose(): void {
+		super.onClose();
+		this.resolve(this.isConfirmed);
+	}
 
-    public override onOpen(): void {
-        super.onOpen();
-        this.titleEl.setText(this.options.title);
-        this.contentEl.createEl('p', { text: this.options.message });
-        const okButton = new ButtonComponent(this.contentEl);
-        okButton.setClass('ok-button');
-        okButton.setButtonText(this.options.okButtonText);
-        okButton.setCta();
-        okButton.onClick(() => {
-            this.isConfirmed = true;
-            this.close();
-        });
+	public override onOpen(): void {
+		super.onOpen();
+		this.titleEl.setText(this.options.title);
+		this.contentEl.createEl("p", { text: this.options.message });
+		const okButton = new ButtonComponent(this.contentEl);
+		okButton.setClass("ok-button");
+		okButton.setButtonText(this.options.okButtonText);
+		okButton.setCta();
+		okButton.onClick(() => {
+			this.isConfirmed = true;
+			this.close();
+		});
 
-        const cancelButton = new ButtonComponent(this.contentEl);
-        cancelButton.setButtonText(this.options.cancelButtonText);
-        cancelButton.onClick(this.close.bind(this));
-    }
+		const cancelButton = new ButtonComponent(this.contentEl);
+		cancelButton.setButtonText(this.options.cancelButtonText);
+		cancelButton.onClick(this.close.bind(this));
+	}
 }
 
 export async function confirm(options: ConfirmOptions): Promise<boolean> {
-    return await new Promise<boolean>((resolve) => {
-        const modal = new ConfirmModal(options, resolve);
-        modal.open();
-    });
+	return await new Promise<boolean>((resolve) => {
+		const modal = new ConfirmModal(options, resolve);
+		modal.open();
+	});
 }
